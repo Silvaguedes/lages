7e2c27cc88b79889342602a205b5db4d2b9cb866
silvaguedes <aldairguedessilvao2gmail.com>
2024-11-16 13:04:25 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const animatedText = document.querySelector('.animated-text');
+const words = ["Desenvolvedor FullStack", "Em fase de transição", "buscando trazer soluções web", "Em busca de uma oportunidade no mercado de tecnologia."];
+let wordIndex = 0;
+
+function changeWord() {
+    animatedText.textContent = words[wordIndex];
+    wordIndex = (wordIndex + 1) % words.length; // Alterna as palavras em loop
+}
+
+setInterval(changeWord, 4000); // Troca a palavra a cada 4 segundos
